4845c878ddc8580b5c5668b1ddea9f4cdf9181fe
Guilherme <guizolas.52@gmail.com>
2020-11-25 22:04:17 -0300

    init

---
+++ src/main/java/br/com/ApiRestConsumer/JsonPlaceHolder/JsonPlaceHolderApplication.java
@@ -0,0 +1,13 @@
+package br.com.ApiRestConsumer.JsonPlaceHolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceHolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPlaceHolderApplication.class, args);
+	}
+
+}
